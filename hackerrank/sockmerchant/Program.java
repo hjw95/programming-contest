68a574b7cf27837f174008d584646a045c840c45
Hady Willi <hadyjanuar95@gmail.com>
2019-05-01 16:18:23 +0800

    Solve sock merchant

---
+++ hackerrank/sockmerchant/Program.java
@@ -0,0 +1,32 @@
+package hackerrank.sockmerchant;
+
+import java.io.*;
+import java.util.*;
+
+public class Program {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String nLine = br.readLine();
+        int n = Integer.parseInt(nLine);
+
+        String arLine = br.readLine();
+        String[] arLineTokens = arLine.trim().split("\\s+");
+
+        int pairCount = 0;
+        Map<Integer, Boolean> currentOdd = new HashMap<Integer, Boolean>();
+        for (String currentArToken : arLineTokens) {
+            int ari = Integer.parseInt(currentArToken);
+            Boolean ariOdd = currentOdd.get(ari);
+            if (ariOdd == null || (!ariOdd)) {
+                currentOdd.put(ari, true);
+            } else {
+                pairCount += 1;
+                currentOdd.put(ari, false);
+            }
+        }
+
+        System.out.println(pairCount);
+    }
+}
